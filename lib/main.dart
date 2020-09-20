96416e54eddc18f21f82664b5c5b2c4e8909b465
Lukman-PC <luckmanhakim004@gmail.com>
2020-09-20 20:42:34 +0700

    home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,119 +16,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Sisi Kiri
-            Stack(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      width: 1280 / 2,
-                      height: 832,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 1280 / 2,
-                      height: 832,
-                      color: Colors.blue,
-                    ),
-                  ],
-                ),
-                // jancokkk
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 100),
-                      width: 75,
-                      height: 80,
-                      color: Colors.black,
-                      child: Center(
-                        child: Text("Logo",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            )),
-                      ),
-                    ),
-                    Container(
-                      height: 116,
-                    ),
-                    Container(
-                      width: 500,
-                      height: 400,
-                      color: Colors.white,
-                      child: Column(
-                        children: [
-                          Text("Bersama Melawan Covid-19"),
-                          Text(
-                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 100,
-                    ),
-                    Container(
-                      width: 221.65,
-                      height: 70,
-                      color: Colors.white,
-                      child: Center(child: Text('Learn More')),
-                    )
-                  ],
-                ),
-                // Sisi Kanan
-                Stack(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 700),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 100,
-                            color: Colors.white,
-                            child: Center(child: Text("Kontol")),
-                          ),
-                          Container(
-                            height: 50,
-                            width: 100,
-                            color: Colors.white,
-                            child: Center(child: Text("Kontol")),
-                          ),
-                          Container(
-                            height: 50,
-                            width: 100,
-                            color: Colors.white,
-                            child: Center(child: Text("Kontol")),
-                          ),
-                          Container(
-                            height: 50,
-                            width: 100,
-                            color: Colors.white,
-                            child: Center(child: Text("Kontol")),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
     );
   }
 }
